154a987c4813ec4a9eb3ccd58324c6220b9998cc
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-06-01 14:45:07 +0300

    chore: initialize React project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Daily Insights</h1>
+      <p>Інсайти на кожен день з маленькою дією.</p>
+    </main>
+  );
+}
+
+export default App;
